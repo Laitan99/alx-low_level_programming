f22046009b67b5dcbda1b55e0b0f1d05a6493a1b
Laitan99 <horlaitanbalogun9@gmail.com>
2022-09-08 11:49:53 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - printing
- *
- * Return: Always 0 (Successful)
- */
+* main - Printing
+*
+* Return: Always 0 (Successful)
+*/
 int main(void)
 {
-	puts("\"programming is like a multilingualpuzzle);
-	return (0);
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
