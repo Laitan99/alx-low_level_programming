cb978e5508f253ccab90a6b002cd867aff2c8871
Laitan99 <horlaitanbalogun9@gmail.com>
2022-09-09 06:03:35 -0700

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,23 @@
-#include <unistd.h>
+#include <stdio.h>
+
 /**
-* main - Prints a text
-*
-* Return: Always 1 (Success)
-*/
+ * main - prints to string
+ * Description: Prints "and that piece of art is useful.." without puts
+ * Return: 1
+ */
+
 int main(void)
 {
-  write(2, "and that piece of art is useful\" - Dora Korpar,2015-10-19\n", 59);
-  return (1);
+char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+long l = 59;
+long fd = 1;
+long syscall = 1;
+long ret = 0;
+__asm__ ("syscall"
+: "=a" (ret)
+: "a" (syscall),
+"D" (fd),
+"S" (s),
+"d" (l));
+return (1);
 }
