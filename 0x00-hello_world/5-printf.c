d787e4c990d119d472678bd55845ade1cd761446
Laitan99 <horlaitanbalogun9@gmail.com>
2022-09-09 05:56:50 -0700

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
-* main - Prints a text
-*
-* Return: Always 0 (Success)
-*/
+ * main - Print a string
+ * Description: use printf
+ * Return: 0
+ */
+
 int main(void)
 {
-  printf("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+	printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
