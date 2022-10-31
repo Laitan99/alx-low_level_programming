71b91eb2f7a904198be021b130a8aa2e06ba60af
Laitan99 <horlaitanbalogun9@gmail.com>
2022-10-31 06:13:26 -0700

    TASK 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,44 @@
-#include "holberton.h"
+#include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
- *  * read_textfile - a function that reads a text file and prints it
- *   *                to POSIX standard output.
- *    *
- *     * @filename: is the file to read
- *      * @letters: number of letters to read and print from file
- *       *
- *        * Return: 0 if it fails or actual number of letters it could
- *         *         read and print
- *         */
+ *  * read_textfile - reads a text file and prints it to the POSIX standard output
+ *   * @filename: name of the file to read
+ *    * @letters: number of letters it should read and print
+ *     *
+ *      * Return: actual number of letters it could read and print
+ *       */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-		int file;
-			ssize_t read_check, wcount;
+		int fd;
+			ssize_t lenr, lenw;
 				char *buffer;
 
-					if (filename == NULL) /*check if file is present*/
+					if (filename == NULL)
 								return (0);
-
-						file = open(filename, O_RDONLY); /*open file*/
-
-							if (file == -1)
+						fd = open(filename, O_RDONLY);
+							if (fd == -1)
 										return (0);
-
-								/*get the size of buffer from number of letters*/
 								buffer = malloc(sizeof(char) * letters);
 									if (buffer == NULL)
 											{
-														free(buffer);
+														close(fd);
 																return (0);
 																	}
-
-										read_check = read(file, buffer, letters); /*read file*/
-											if (read_check == -1) /*check if read failed*/
-														return (0);
-
-												wcount = write(STDOUT_FILENO, buffer, read_check); /*write to POSIX*/
-													if (wcount == -1 || read_check != wcount) /*check if write failed*/
-																return (0);
-
+										lenr = read(fd, buffer, letters);
+											close(fd);
+												if (lenr == -1)
+														{
+																	free(buffer);
+																			return (0);
+																				}
+													lenw = write(STDOUT_FILENO, buffer, lenr);
 														free(buffer);
-
-															close(file); /*close file*/
-
-																return (wcount);
+															if (lenr != lenw)
+																		return (0);
+																return (lenw);
 }
