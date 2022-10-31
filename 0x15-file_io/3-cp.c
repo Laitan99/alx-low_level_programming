de9f721dad7c486278366aa445bd145ff47bfabd
Laitan99 <horlaitanbalogun9@gmail.com>
2022-10-31 06:21:33 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,86 +1,72 @@
-#include "holberton.h"
-
-#define MAXSIZE 1024
-
+#include "main.h"
+#include <stdio.h>
 
 /**
- *  * __exit - prints error messages and exits with exit number
- *   *
- *    * @error: either the exit number or file descriptor
- *     * @str: name of either file_in or file_out
- *      * @fd: file descriptor
- *       *
- *        * Return: 0 on success
- *        */
-int __exit(int error, char *str, int fd)
+ *  * error_file - checks if files can be opened.
+ *   * @file_from: file_from.
+ *    * @file_to: file_to.
+ *     * @argv: arguments vector.
+ *      * Return: no return.
+ *       */
+void error_file(int file_from, int file_to, char *argv[])
 {
-		switch (error)
+		if (file_from == -1)
 				{
-							case 97:
-											dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-														exit(error);
-																case 98:
-																	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", s);
-																				exit(error);
-																						case 99:
-																							dprintf(STDERR_FILENO, "Error: Can't write to %s\n", s);
-																										exit(error);
-																												case 100:
-																													dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
-																																exit(error);
-																																		default:
-																																			return (0);
-																																				}
+							dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+									exit(98);
+										}
+			if (file_to == -1)
+					{
+								dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+										exit(99);
+											}
 }
 
 /**
- *  * main - create a copy of file
- *   *
- *    * @argc: argument counter
- *     * @argv: argument vector
- *      *
- *       * Return: 0 for success.
- *       */
+ *  * main - check the code for Holberton School students.
+ *   * @argc: number of arguments.
+ *    * @argv: arguments vector.
+ *     * Return: Always 0.
+ *      */
 int main(int argc, char *argv[])
 {
-		int file_in, file_out;
-			int read_stat, write_stat;
-				int close_in, close_out;
-					char buffer[MAXSIZE];
+		int file_from, file_to, err_close;
+			ssize_t nchars, nwr;
+				char buf[1024];
 
-						/*if arguments are not 3*/
-						if (argc != 3)
-									__exit(97, NULL, 0);
+					if (argc != 3)
+							{
+										dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+												exit(97);
+													}
 
-							/*sets file descriptor for copy from file*/
-							file_in = open(argv[1], O_RDONLY);
-								if (file_in == -1)
-											__exit(98, argv[1], 0);
+						file_from = open(argv[1], O_RDONLY);
+							file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+								error_file(file_from, file_to, argv);
 
-									/*sets file descriptor for copy to file*/
-									file_out = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
-										if (file_out == -1)
-													__exit(99, argv[2], 0);
+									nchars = 1024;
+										while (nchars == 1024)
+												{
+															nchars = read(file_from, buf, 1024);
+																	if (nchars == -1)
+																					error_file(-1, 0, argv);
+																			nwr = write(file_to, buf, nchars);
+																					if (nwr == -1)
+																									error_file(0, -1, argv);
+																						}
 
-											/*reads file_in as long as its not NULL*/
-											while ((read_stat = read(file_in, buffer, MAXSIZE)) != 0)
-													{
-																if (read_stat == -1)
-																				__exit(98, argv[1], 0);
+											err_close = close(file_from);
+												if (err_close == -1)
+														{
+																	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+																			exit(100);
+																				}
 
-																		/*copy and write contents to file_out*/
-																		write_stat = write(file_out, buffer, read_stat);
-																				if (write_stat == -1)
-																								__exit(99, argv[2], 0);
-																					}
-
-												close_in = close(file_in); /*close file_in*/
-													if (close_in == -1)
-																__exit(100, NULL, file_in);
-
-														close_out = close(file_out); /*close file_out*/
-															if (close_out == -1)
-																		__exit(100, NULL, file_out);
-
-																return (0);
+													err_close = close(file_to);
+														if (err_close == -1)
+																{
+																			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+																					exit(100);
+																						}
+															return (0);
 }
