b7f09d9337c2fec8a99350879705e35d0e2e297a
Laitan99 <horlaitanbalogun9@gmail.com>
2022-11-01 01:10:08 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,44 +1,44 @@
 #include "main.h"
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
 
 /**
- *  * read_textfile - reads a text file and prints it to the POSIX standard output
- *   * @filename: name of the file to read
- *    * @letters: number of letters it should read and print
- *     *
- *      * Return: actual number of letters it could read and print
- *       */
+ * * read_textfile - check the code for Holberton School students.
+ * * @filename: file to read and write
+ * * @letters: number of letters to read and write.
+ * * Return: letters printed
+ * */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-		int fd;
-			ssize_t lenr, lenw;
-				char *buffer;
+		ssize_t nletters;
+			int file;
+				char *text;
 
-					if (filename == NULL)
+					if (!filename)
 								return (0);
-						fd = open(filename, O_RDONLY);
-							if (fd == -1)
+						text = malloc(sizeof(char) * letters + 1);
+							if (text == NULL)
 										return (0);
-								buffer = malloc(sizeof(char) * letters);
-									if (buffer == NULL)
+								file = open(filename, O_RDONLY);
+									if (file == -1)
 											{
-														close(fd);
+														free(text);
 																return (0);
 																	}
-										lenr = read(fd, buffer, letters);
-											close(fd);
-												if (lenr == -1)
-														{
-																	free(buffer);
-																			return (0);
-																				}
-													lenw = write(STDOUT_FILENO, buffer, lenr);
-														free(buffer);
-															if (lenr != lenw)
-																		return (0);
-																return (lenw);
+										nletters = read(file, text, sizeof(char) * letters);
+											if (nletters == -1)
+													{
+																free(text);
+																		close(file);
+																				return (0);
+																					}
+												nletters = write(STDOUT_FILENO, text, nletters);
+													if (nletters == -1)
+															{
+																		free(text);
+																				close(file);
+																						return (0);
+																							}
+														free(text);
+															close(file);
+																return (nletters);
 }
